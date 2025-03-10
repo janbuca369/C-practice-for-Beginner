79de34ac380cc96ffe9044de3593bc7ab079652a
janbuca369 <firewkifle2@gmail.com>
2025-03-10 12:06:55 +0300

    Second file can be add

---
+++ C++ practice for Beginner/2 C++/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Name \t\t ID \t\t Grade" << endl;
+    cout << "Janbuca \t uu79309 \t 10+7" << endl;
+    cout << "Mimo \t\t uu80280 \t 10+2" << endl;
+    cout << "Sami \t\t uu90290 \t 10+3" << endl;
+    return 0;
+}
