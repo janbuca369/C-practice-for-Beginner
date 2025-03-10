6393d6364ad7d99f88ebf026439adf5ff8f84b51
janbuca369 <firewkifle2@gmail.com>
2025-03-10 12:18:47 +0300

    Increment/decrement operator

---
+++ C++ practice for Beginner/8 C++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/*
+    Increment/decrement operator (--, ++)
+        - Prefix: type is written before the variable (++myAge).
+        - Postfix: type appears after the variable name(myAge++)
+*/
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
