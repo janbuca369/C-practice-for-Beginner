f2d7f38e3e4adeda56a4cab19545a5c953485676
janbuca369 <firewkifle2@gmail.com>
2025-03-10 11:51:42 +0300

    firest line can be add

---
+++ C++ practice for Beginner/1 C++/main.cpp
@@ -0,0 +1,20 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Janbuca pis new world!" << endl;
+    return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hey all student! "<<endl;
+    return 0;
+}
