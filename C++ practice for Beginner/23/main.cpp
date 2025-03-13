666ef425d7cc21bb0be8784e57d7c960b933e2b2
janbuca369 <firewkifle2@gmail.com>
2025-03-13 12:09:37 +0300

    defualt function

---
+++ C++ practice for Beginner/23/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout<<sqrt(9)<<endl;
+    cout<<abs(-16)<<endl;
+    cout<<fabs(-16.89)<<endl;
+    cout<<pow(-4, 2)<<endl;
+    cout<<sin(90)<<endl;
+    cout<<tolower('A')<<endl;
+    cout<<toupper('z')<<endl;
+    cout<<floor(9.97)<<endl;
+    return 0;
+}
