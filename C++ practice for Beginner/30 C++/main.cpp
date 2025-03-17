2c9a1a6de671cfad0c8a2c3970997545a3fa85fe
janbuca369 <firewkifle2@gmail.com>
2025-03-17 10:07:24 +0300

    One dimational array

---
+++ C++ practice for Beginner/30 C++/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+/*
+    Array
+*/
+using namespace std;
+
+int main()
+{
+    /*
+    int odd[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
+    cout<<odd[0]<<" ";
+    cout<<odd[1]<<" ";
+    cout<<odd[2]<<" ";
+    cout<<odd[3]<<" ";
+    cout<<odd[4]<<" ";
+    cout<<odd[5]<<" ";
+    */
+
+
+    /*
+    float mark[] = {89, 67, 90, 77, 99};
+    cout<<"Enter new mark \n";
+    cin>>mark[3]; //100
+    cout<<mark[3]<<endl;
+    */
+
+    /*
+
+    // Accessing the whole Elements of the array
+    int odd[] = {1, 3, 5, 7, 8, 11};
+    for(int i=0; i<6; i++){
+        cout<<odd[i]<<endl;
+    }
+
+    int mark[] = {78, 45, 67, 34, 89};
+    for(int i=0; i<5; i++){
+        cout<<mark[i]<<" ";
+    }
+
+    */
+
+    /*
+    // Write a program which asks the user to type 3 float number and display th smallest one.
+    int n[5];
+    cout<<"Enter five integer number: ";
+    for(int j=0; j<5; j++){
+        cin>>n[j];
+    }
+
+    int small = n[0];
+    for(int i=0; i<5; i++){
+        if(n[i]<small){
+            small=n[i];
+        }
+    }
+    cout<<endl<<" \n The Smallest number is:"<<small<<endl;
+    */
+
+
+    /*
+    //Write a program which accepts 5 integer values and display them in a reverse order.
+    int num[5];
+    cout<<"Enter five integer number: \n";
+    for(int j=0; j<5; j++){
+        cin>>num[j];
+    }
+    for(int i=5; i>=0; i--){
+        cout<<num[i]<<" ";
+    }
+
+    */
+
+
+    /*
+    //Searching number
+    int n[] = {11, 22, 33, 44, 55, 66, 77, 88, 99, 00};
+    int s;
+    cout<<"Enter a number to search: \n";
+    cin>>s;
+    for(int i=0; i<10; i++){
+        if(n[i] == s){
+            cout<<"The number is found and it is: "<<n[i]<<endl;
+        }else{
+            cout<<"The number is not't found.\n";
+        }
+    }
+    */
+
+    //Write a program which reads 7 integers and display their sum using array.
+    return 0;
+}
