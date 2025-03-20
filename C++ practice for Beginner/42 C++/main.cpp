62fe42a661db2add75ac5ceabe84b4c092b453e3
janbuca369 <firewkifle2@gmail.com>
2025-03-20 12:07:50 +0300

    Pointer and String

---
+++ C++ practice for Beginner/42 C++/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// Pointer and String
+using namespace std;
+
+int main()
+{
+    /*char str[] = "Janbuca Teshome";
+    cout<<str<<endl;
+    // str++; illgal
+
+
+    char *str1 = "I Love Ethiopia";
+    cout<<str1<<endl;
+    str1++;
+    cout<<str1;
+    */
+
+
+    return 0;
+}
